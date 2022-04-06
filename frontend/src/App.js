45587fdb002e2c1d53cff896cffda2988ec95cb4
Mikhail Davydov <mkhldvdv@gmail.com>
2022-04-07 02:09:08 +0300

    chore: minor changes in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import {Component} from "react";
 
 class App extends Component {
-  render() {
-    return (
-        <div className="main">
-          main here!
-        </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="main">
+                main here!
+            </div>
+        );
+    }
+
 }
 
 export default App;
