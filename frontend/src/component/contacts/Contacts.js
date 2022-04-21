b5612b7e4ebe29a4b83f40d5b7a0ae3e78a50819
Mikhail Davydov <mkhldvdv@gmail.com>
2022-04-22 01:35:18 +0300

    BDR-20: contacts component: phone, email, telegram, vk icons list

--- frontend/src/component/contacts/Contacts.js
+++ frontend/src/component/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import Icon from "../icon/Icon";
+import {email, phone, telegram, vk} from "../common/Constants";
 
 import "./Contacts.css";
 
@@ -11,45 +11,34 @@ export class Contacts extends Component {
             contacts: [
                 {
                     key: 1,
-                    content: "call"
+                    content: phone
                 },
                 {
                     key: 2,
-                    content: "email"
+                    content: email
                 },
                 {
                     key: 3,
-                    content: "telegram"
+                    content: telegram
                 },
                 {
                     key: 4,
-                    content: "vk"
+                    content: vk
                 },
             ]
         }
     }
 
     render() {
-        let iconContent =
-            <svg xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 122.88 122.88">
-                <path className="cls-1"
-                      d="M61.44,0A61.44,61.44,0,1,1,0,61.44,61.44,61.44,0,0,1,61.44,0ZM30.73,38,62,63.47,91.91,38Zm-2,42.89L51,58.55,28.71,40.39V80.87ZM53.43,60.55l-22.95,23H92.21l-21.94-23L63,66.71h0a1.57,1.57,0,0,1-2,0l-7.59-6.19Zm19.24-2,21.5,22.54V40.19L72.67,58.51Z"/>
-            </svg>;
-
         let contacts = this.state.contacts.map(contact => {
-            // let iconContent =
-            //     <svg xmlns="http://www.w3.org/2000/svg"
-            //          viewBox="0 0 122.88 122.88">
-            //         <path className="cls-1"
-            //               d={contact.content}/>
-            //     </svg>;
             return (
-                <li className="contacts__item" key={contact.key}>
-                    <a className="contacts__link" href="#">
-                        <Icon class="contacts__item-icon" content={iconContent}/>
-                    </a>
-                </li>
+                <a className="contacts__link" href="#" key={contact.key}>
+                    <li className="contacts__item">
+                        <div className="contacts__item-round icon">
+                            {contact.content}
+                        </div>
+                    </li>
+                </a>
             )
         });
         return (
