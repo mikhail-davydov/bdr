30fd92c0ec29f06aab53ff7da7384503986cc444
Mikhail Davydov <mkhldvdv@gmail.com>
2022-04-23 01:09:25 +0300

    BDR-21: nav layout, menu, items list

--- frontend/src/component/contacts/Contacts.js
+++ frontend/src/component/contacts/Contacts.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import {emailIcon, phoneIcon, telegramIcon, vkIcon} from "../common/Constants";
+import {emailIcon, phoneIcon, telegramIcon, vkIcon} from "./ContactIcons";
 
 import "./Contacts.css";
 
