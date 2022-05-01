ee1689c71db94783e3a79588c34045ba1daa847f
Mikhail Davydov <mkhldvdv@gmail.com>
2022-05-02 02:04:27 +0300

    BDR-28: initial payment form refactoring

--- frontend/src/component/payment/Payment.js
+++ frontend/src/component/payment/Payment.js
@@ -6,29 +6,45 @@ export class Payment extends Component {
 
     render() {
         return (
-            <div className="payment-container">
+            <div className={"payment-container"}>
                 <form action="/some-link-here" className="payment-form">
-                    <input type="text" className="payment-form__comment" defaultValue="Комментарий"/>
-                    <select name="form-select" id="form-select" className="payment-form__select-category">
-                        <option value="" className="payment-form__select-option" selected disabled>Выбрать категорию
-                        </option>
-                        <option value="1" className="payment-form__select-option">ООО Ромашка</option>
-                        <option value="2" className="payment-form__select-option">Компания 1</option>
-                    </select>
-                    <input type="date" className="payment-form__date"/>
-                    <input type="number" className="payment-form__amount"/>
-                    <div>
-                        <label>
-                            <input type="radio" className="payment-form__sign" name="sign" value="+" checked/>
-                            +
-                        </label>
-                        <label>
-                            <input type="radio" className="payment-form__sign" name="sign" value="-"/>
-                            -
-                        </label>
-                    </div>
-                    <button className="payment-form__submit">Создать</button>
+                    <ul className={"payment-form__list"}>
+                        <li className="payment-form__item">
+                            <input className="payment-form__comment" name={"comment"} type="text"
+                                   placeholder={"Комментарий"}/>
+                        </li>
+                        <li className="payment-form__item">
+                            <select className="payment-form__select-category" name={"category"} id="form-select">
+                                <option value="" className="payment-form__select-option" selected disabled>Выбрать
+                                    категорию
+                                </option>
+                                <option value="1" className="payment-form__select-option">Расходы</option>
+                                <option value="2" className="payment-form__select-option">Новая категория</option>
+                            </select>
+                        </li>
+                        <li className="payment-form__item">
+                            <input className="payment-form__date" name={"date"} type="date"/>
+                        </li>
+                        <li className="payment-form__item">
+                            <input className="payment-form__amount" name={"amount"} type="number"/>
+                        </li>
+                        <li className="payment-form__item">
+                            <fieldset className={"payment-form__fieldset"}>
+                                <input className="payment-form__sign" value="-" name={"sign"} type="radio" id={"minus"}
+                                       defaultChecked/>
+                                <label className="payment-form__label" htmlFor={"minus"}>-</label>
+                                <input className="payment-form__sign" value="+" name={"sign"} type="radio" id={"plus"}/>
+                                <label className="payment-form__label" htmlFor={"plus"}>+</label>
+                            </fieldset>
+                        </li>
+                        <li className="payment-form__item">
+                            <button className="payment-form__submit" type={"button"}>Создать</button>
+                        </li>
+                    </ul>
                 </form>
+                <div className="payment-content">
+                    Here is the payment content
+                </div>
             </div>
         );
     }
