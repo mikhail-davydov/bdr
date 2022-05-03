9b4258167a11cd591eec1b7ad27c93c1e3e25dfc
Mikhail Davydov <mkhldvdv@gmail.com>
2022-05-04 02:21:59 +0300

    BDR-28: payment form, refactor and style

--- frontend/src/component/payment/Payment.js
+++ frontend/src/component/payment/Payment.js
@@ -10,16 +10,14 @@ export class Payment extends Component {
                 <form action="/some-link-here" className="payment-form">
                     <ul className={"payment-form__list"}>
                         <li className="payment-form__item">
-                            <input className="payment-form__comment" name={"comment"} type="text"
-                                   placeholder={"Комментарий"}/>
-                        </li>
-                        <li className="payment-form__item">
                             <select className="payment-form__select-category" name={"category"} id="form-select">
                                 <option value="" className="payment-form__select-option" selected disabled>Выбрать
                                     категорию
                                 </option>
                                 <option value="1" className="payment-form__select-option">Расходы</option>
-                                <option value="2" className="payment-form__select-option">Новая категория</option>
+                                <option value="2" className="payment-form__select-option">Новая (очень длинная)
+                                    категория
+                                </option>
                             </select>
                         </li>
                         <li className="payment-form__item">
@@ -29,16 +27,20 @@ export class Payment extends Component {
                             <input className="payment-form__amount" name={"amount"} type="number" step={"0.01"}/>
                         </li>
                         <li className="payment-form__item">
-                            <fieldset className={"payment-form__fieldset"}>
-                                <input className="payment-form__sign" value="-" name={"sign"} type="radio" id={"minus"}
-                                       defaultChecked/>
-                                <label className="payment-form__label" htmlFor={"minus"}>-</label>
-                                <input className="payment-form__sign" value="+" name={"sign"} type="radio" id={"plus"}/>
-                                <label className="payment-form__label" htmlFor={"plus"}>+</label>
-                            </fieldset>
+                            <label className="payment-form__checkbox-label">
+                                <span className="payment-form__checkbox-text">Расход</span>
+                                <div className="payment-form__checkbox-container">
+                                    <input className="payment-form__checkbox" type="checkbox" name="positive"/>
+                                </div>
+                                <span className="payment-form__checkbox-text">Приход</span>
+                            </label>
                         </li>
                         <li className="payment-form__item">
-                            <button className="payment-form__submit" type={"button"}>Создать</button>
+                            <input className="payment-form__comment" name={"comment"} type="text"
+                                   placeholder={"Комментарий"}/>
+                        </li>
+                        <li className="payment-form__item">
+                            <button className="button payment-form__submit" type={"button"}>Создать</button>
                         </li>
                     </ul>
                 </form>
